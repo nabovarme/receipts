89c283b1ff9e59dcab6ff756750537b1ba5cd5ca
nihil <666@nabovarme.dk>
2018-12-17 00:40:34 +0100

    fuck det her git pis

--- ocr.py
+++ ocr.py
@@ -25,7 +25,6 @@ dbconfig = {
 }
     # Connect to the database
 CONNECTION = pymysql.connect(
-                                db='db',
                                 charset='utf8mb4',
                                 cursorclass=pymysql.cursors.DictCursor, **dbconfig)
 
